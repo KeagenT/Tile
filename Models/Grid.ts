964c42020abcec960dfb15aa8b2c5b7bc43f3793
Keagen Thomson <thomsonkeagen@gmail.com>
2024-11-19 12:39:29 -0500

    updating docs of Grid and Readme.MD

--- Models/Grid.ts
+++ Models/Grid.ts
@@ -1,7 +1,49 @@
-import { HoleTile, Tile } from "./Tile.ts";
-import { Position } from "./Position.ts";
+import { HoleTile, type Tile } from "./Tile.ts";
+import type { Position } from "./Position.ts";
 
-export class Grid<T> {
+interface GridBase<T> {
+    /** Gets the object located at the (X,Y) position of the grid. 
+     * e.g. 
+     * ```ts
+     * TileGrid.get(new Position(0, 0)) => HoleTile(...)
+     * ```
+    */
+    get(p: Position): T | undefined;
+    /** Sets the (X,Y) position of the grid to a specified object 
+    * e.g. 
+    * ```ts
+    * TileGrid.set(new Position(0, 0), new GroundTile(...));
+    * TileGrid.get(new Position(0, 0)) => GroundTile(...);
+    * ```
+    */
+    set(p: Position, value: T): void;
+    /** The actual grid which is a 2D array of specified generic type */
+    grid(): T[][];
+    /** The horizontal length of the grid, including empty spots */
+    get width(): number;
+    /** The horizontal length of the grid, including empty spots */
+    get height(): number;
+    /** 
+     * Determines if the provided (X, Y) position is valid and within the grid. Used for boundary detection 
+     * e.g.
+     * ```ts
+     * Grid.width() => 3;
+     * Grid.height() => 3;
+     * Grid.contains(new Position(5, 5)) => false;
+     * Grid.contains(new Position(2, 2)) => true;
+     * ```
+     * */
+    contains(p: Position): boolean;
+
+}
+
+/**
+ * A generic grid of X width and Y height,
+ * with utility methods to get and set objects at a position,
+ * and determine if a position is contained within the grid.
+ * *note* The grid is 0-indexed and the Y axis "starts" from the top of the screen.
+ */
+export class Grid<T>  implements GridBase<T> {
     private _grid: T[][];
     private _width: number;
     private _height: number;
@@ -56,7 +98,7 @@ export class TileGrid extends Grid<Tile> {
         super(width, height, () => new HoleTile());
     }
 
-    public toString(): string {
+    public override toString(): string {
         return this.grid().map(row => row.map(tile => tile.toString()).join('')).join('\n');
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Readme
 
 tilelib-2d is a quick and dirty collection of utility classes and models I used to make this [Auto Tiling Component](keagentcreative.com/blog/auto-tiling-1). Feel free to use it however you'd like, you'll find it most useful though in 2D Tile Based Map making.
+
+## Contents
+
+The Package is organized in three groups `Models`, `Commands`, and `Utilities`. 
+
+### Commands
+
+Commands contains the extendable `Command` base class used to implement the command pattern, which is really just a fancy way of making a function call into an object with a generic `.execute()` call to run, and a generic `.undo()`, to undo said command. Why use a command? [Read this](https://gameprogrammingpatterns.com/command.html). They're very useful when you want something done on an input, you want to customize the input source regardless of platform, and you want to possibly take it back in the future.
+
+```ts
+ export abstract class Command {
+    execute(): void {};
+    undo(): void {};
+}
+```
+
+There are a few predefined commands concerning setting tile types on a 2d grid, updating their bitmask based on surrounding tile-types, and a bundled command called "draw" which executes these sub-commands on a user interaction with the tile grid. Feel free to make anything you'd like.
+
+### Models
+
+Models contain essential classes needed for basic 2D tile grids including the generic 2D array `Grid` class, `TileGrid` class, `Position` class,
+and of course the `Tile` class. 
